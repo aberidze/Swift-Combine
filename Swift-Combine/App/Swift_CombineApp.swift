79211f4ae7c42e65983cdfffe93adc29718e74a6
aberidze <aberidze02@gmail.com>
2026-07-10 16:16:24 +0400

    initial xcode project setup commit
    Add starter XCode project

---
+++ Swift-Combine/App/Swift_CombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_CombineApp.swift
+//  Swift-Combine
+//
+//  Created by Anri Beridze on 10/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct Swift_CombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
